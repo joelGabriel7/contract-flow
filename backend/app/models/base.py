677a08e06dcee25f1650e0e948416b3f29fa5895
Joel German <joelgerman671@gmail.com>
2025-02-14 03:14:18 -0400

    Revert "Moves files into folder backend"
    This reverts commit da62b9264bfe19171caeb3fbc7c64498bb7d4f6d.

--- backend/app/models/base.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from typing import Optional
-from sqlmodel import SQLModel, Field
-
-
-class TimestampModel(SQLModel):
-    created_at: datetime  = Field(default_factory=datetime.utcnow)
-    updated_at: Optional[datetime]  = Field(default=None)
